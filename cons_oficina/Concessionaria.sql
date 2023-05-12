4066b9a44d82d03cee75ff271d13b00a242e7621
Fabio Ribas dos Santos <fabyo_27@hotmail.com>
2023-05-11 22:20:43 -0300

    alterado

--- cons_oficina/Concessionaria.sql
+++ cons_oficina/Concessionaria.sql
@@ -1,16 +1,16 @@
 CREATE DATABASE concessionaria;
-use concessionaria;
+USE concessionaria;
 CREATE TABLE cad_oficina (
   nome VARCHAR(50) NOT NULL,
-  cpf number(11) NOT NULL,
-  endereco INT NOT NULL,
+  cpf BIGINT NOT NULL,
+  endereco VARCHAR(255) NOT NULL,
   email VARCHAR(50) NOT NULL,
   fabricante VARCHAR(50) NOT NULL,
   modelo VARCHAR(50) NOT NULL,
   ano VARCHAR(50) NOT NULL,
   cor VARCHAR(50) NOT NULL,
   placa VARCHAR(50) NOT NULL,
-  observacoes TEXT NOT NULL  
-  )ENGINE=INNODB;
-  
-select * from cad_oficina;
+  observacoes TEXT NOT NULL
+) ENGINE=InnoDB;
+
+SELECT * FROM cad_oficina;
--- cons_oficina/verifica.php
+++ cons_oficina/verifica.php
@@ -2,66 +2,77 @@
 
 include_once("conecta_mysql.inc");
 
-$nome 			 	= $_POST['nome'];
-$cpf	  	 	 	= $_POST['cpf'];
-$endereco	  		= $_POST['endereco'];
-$email  		 	= $_POST['email'];
-$fabricante 		= $_POST['fabricante'];
-$modelo				= $_POST['modelo'];
-$ano 				= $_POST['ano'];
-$cor 				= $_POST['cor'];
-$placa				= $_POST['placa'];
-$observacoes	  	= $_POST['observacoes'];
-$erro 			 	= 0;
+$nome 		 = $_POST['nome'];
+$cpf	  	 = $_POST['cpf'];
+$endereco	 = $_POST['endereco'];
+$email  	 = $_POST['email'];
+$fabricante  = $_POST['fabricante'];
+$modelo		 = $_POST['modelo'];
+$ano 		 = $_POST['ano'];
+$cor 		 = $_POST['cor'];
+$placa		 = $_POST['placa'];
+$observacoes = $_POST['observacoes'];
+$erro 		 = 0;
 
-if (empty($nome) OR strstr($nome, ' ')==false) {
+if (empty($nome) || strstr($nome, ' ') == false) {
 	echo "Favor digitar o seu nome corretamente.<br>";
 	$erro = 1;
 }
 
-if (empty($cpf) ==false) {
-	echo "Favor digitar o seu cpf corretamente.<br>";
+if (empty($cpf) || !is_numeric($cpf) || strlen($cpf) != 11) {
+	echo "Favor digitar o seu CPF corretamente.<br>";
 	$erro = 1;
 }
 
-if (empty($endereco) ==false) {
-	echo "Favor digitar o seu nome corretamente.<br>";
+if (empty($endereco)) {
+	echo "Favor digitar o seu endereço corretamente.<br>";
 	$erro = 1;
 }
 
-if (empty($email) ==false) {
-	echo "Favor digitar o seu nome corretamente.<br>";
+if (empty($email) || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+	echo "Favor digitar o seu e-mail corretamente.<br>";
 	$erro = 1;
 }
 
-if (empty($fabricante) ==false) {
-	echo "Favor digitar o seu nome corretamente.<br>";
+if (empty($fabricante)) {
+	echo "Favor digitar o fabricante do veículo corretamente.<br>";
 	$erro = 1;
 }
 
-if (empty($modelo) ==false) {
-	echo "Favor digitar o seu nome corretamente.<br>";
+if (empty($modelo)) {
+	echo "Favor digitar o modelo do veículo corretamente.<br>";
 	$erro = 1;
 }
 
-if (empty($ano) ==false) {
-	echo "Favor digitar o seu nome corretamente.<br>";
+if (empty($ano) || !is_numeric($ano) || strlen($ano) != 4) {
+	echo "Favor digitar o ano do veículo corretamente.<br>";
 	$erro = 1;
 }
 
-if (empty($cor) ==false) {
-	echo "Favor digitar o seu nome corretamente.<br>";
+if (empty($cor)) {
+	echo "Favor digitar a cor do veículo corretamente.<br>";
 	$erro = 1;
 }
 
-if (empty($placa) ==false) {
-	echo "Favor digitar o seu nome corretamente.<br>";
+if (empty($placa)) {
+	echo "Favor digitar a placa do veículo corretamente.<br>";
 	$erro = 1;
 }
 
-if (empty($observacoes) ==false) {
-	echo "Favor digitar o seu nome corretamente.<br>";
+if (empty($observacoes)) {
+	echo "Favor digitar as observações corretamente.<br>";
 	$erro = 1;
 }
 
- ?>
+if ($erro == 0) {
+	$sql = "INSERT INTO cad_oficina VALUES ('$nome','$cpf','$endereco','$email','$fabricante','$modelo','$ano','$cor','$placa','$observacoes')";
+
+	if ($conexao->query($sql) === true) {
+		echo "Usuário incluído com sucesso!";
+	} else {
+		echo "Erro ao incluir usuário: " . $conexao->error;
+	}
+}
+
+$conexao->close();
+?>
